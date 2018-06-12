aa6c416c670effa44b77d294ac8047a688dfc4c8
JasmineCarter <jlcart6@gmail.com>
2018-06-11 20:04:36 -0400

    html not working

---
+++ src/test/java/org/wecancodeit/courses/CourseControllerMockMvcTest.java
@@ -0,0 +1,26 @@
+package org.wecancodeit.courses;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CourseController.class)
+
+public class CourseControllerMockMvcTest {
+	
+	@Resource
+	private MockMvc mvc; 
+	
+	@Test
+	public void shouldBeOkForAllCourses() throws Exception {
+		mvc.perform(get("/show-courses")).andExpect(status().isOk()); 
+}
+}
